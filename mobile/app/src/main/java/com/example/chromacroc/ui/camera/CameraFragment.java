dcddf12e2f0c02fdc689918405d3456d7407e2bb
kuridza1 <131389372+kuridza1@users.noreply.github.com>
2026-03-28 15:45:18 +0100

    [ADD] Startup mobile app

---
+++ mobile/app/src/main/java/com/example/chromacroc/ui/camera/CameraFragment.java
@@ -0,0 +1,44 @@
+package com.example.chromacroc.ui.camera;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.chromacroc.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link CameraFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class CameraFragment extends Fragment {
+
+    public static CameraFragment newInstance(String param1, String param2) {
+        CameraFragment fragment = new CameraFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_camera, container, false);
+    }
+}
